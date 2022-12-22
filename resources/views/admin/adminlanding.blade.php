3d40d5a91d335d179a383340fa7244252de574d5
wafijailani <wafijailani10@live.com>
2022-12-22 17:27:46 +0800

    changed logos on registration

--- resources/views/admin/adminlanding.blade.php
+++ resources/views/admin/adminlanding.blade.php
@@ -7,7 +7,24 @@
 </head>
 <body>
 @include("admin.navbar")
-
+<div class="col-md-6 d-flex align-items-stretch">
+                <div class="row">
+                  <div class="col-md-10 grid-margin">
+                    <div class="card">
+                      <div class="card-body">
+                      <div class="template-demo">
+                      <h1>Welcome to FYP Management System (FYP Coordinator Side)</h1>
+                      <h4 class="card-title">Created by Abdul Wafi Jailani</h4>
+                        </div>
+                        <p>There are a few functions in this system</p>
+                        <p>1. You will be able to view all projects from all students.</p>
+                        <p>2. You will be able to delete projects.</p>
+                        <p>3. You will be able to add new projects with details and assign to supervisor and examiners.</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>    
 @include("admin.script")          
 </body>
 </html>
--- resources/views/staff/stafflanding.blade.php
+++ resources/views/staff/stafflanding.blade.php
@@ -7,7 +7,24 @@
 </head>
 <body>
 @include("staff.staffnavbar")
-
+<div class="col-md-6 d-flex align-items-stretch">
+                <div class="row">
+                  <div class="col-md-10 grid-margin">
+                    <div class="card">
+                      <div class="card-body">
+                      <div class="template-demo">
+                      <h1>Welcome to FYP Management System (Lecturer's Side)</h1>
+                      <h4 class="card-title">Created by Abdul Wafi Jailani</h4>
+                        </div>
+                        <p>There are a few functions in this system</p>
+                        <p>1. You will be able to view students and projects that are assigned under you.</p>
+                        <p>2. You will be able to update project informations if you are assigned as their supervisor.</p>
+                        <p>3. You will be able to view projects that you are assigned as examiner.</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>   
 @include("staff.staffscript")
 </body>
 </html>
